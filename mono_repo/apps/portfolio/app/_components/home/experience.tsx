517ef32f3cb0372a429e04ff594b5e30ea44cae1
varun teja <varunteja007006@gmail.com>
2025-10-10 20:41:31 +0530

    ✅ : minify the exp

--- mono_repo/apps/portfolio/app/_components/home/experience.tsx
+++ mono_repo/apps/portfolio/app/_components/home/experience.tsx
@@ -1,74 +1,138 @@
-import React from "react"
-import experienceData from "@/app/_components/home/data/experience"
-import { Card, CardFooter, CardHeader, CardTitle } from "@workspace/ui/components/card"
-import { Badge } from "@workspace/ui/components/badge"
+import React from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@workspace/ui/components/dialog";
+import { Badge } from "@workspace/ui/components/badge";
+
+import experienceData from "@/app/_components/home/data/experience";
 import {
   addYears,
   differenceInMonths,
   differenceInYears,
   format,
   parse,
-} from "date-fns"
+} from "date-fns";
+import { Maximize2 } from "lucide-react";
+import { Button } from "@workspace/ui/components/button";
 
 function calcExp(date1: string, date2: string) {
-  const resigningDate = new Date(parse(date1, "yyyy-MM-dd", new Date()))
-  const joiningDate = new Date(parse(date2, "yyyy-MM-dd", new Date()))
+  const resigningDate = new Date(parse(date1, "yyyy-MM-dd", new Date()));
+  const joiningDate = new Date(parse(date2, "yyyy-MM-dd", new Date()));
 
   // Calculate the difference in years
-  let years = differenceInYears(resigningDate, joiningDate)
+  let years = differenceInYears(resigningDate, joiningDate);
   // Calculate the remaining months after accounting for the years
-  let months = differenceInMonths(resigningDate, addYears(joiningDate, years))
+  let months = differenceInMonths(resigningDate, addYears(joiningDate, years));
 
   // Handle the case where months are negative
   if (months < 0) {
-    years -= 1
-    months += 12
+    years -= 1;
+    months += 12;
   }
-  return `${years}.${months}`
+  return `${years}.${months}`;
 }
 
 export default function Experience() {
   return (
     <section id="experience" className="space-y-4">
       <h3 className="text-xl font-semibold">Experience</h3>
-      <div className="grid grid-cols-1 gap-4">
-        {experienceData.map(item => {
+
+      <div>
+        {experienceData.map((item, index) => {
           if (!item.toShow) {
-            return null
+            return null;
           }
 
           return (
-            <Card key={item.id}>
-              <CardHeader>
-                <CardTitle className="flex items-center gap-3 text-lg">
-                  {item.jobTitle}{" "}
-                  {item.current && <Badge variant="default" className="font-semibold">Current</Badge>}
-                  {item.current ? (
-                    <Badge variant="outline" className="font-semibold">
-                      Exp:{" "}
-                      {calcExp(
-                        format(new Date(), "yyyy-MM-dd"),
-                        item.dateOfJoining
-                      )}{" "}
-                      yrs
-                    </Badge>
-                  ) : (
-                    <Badge variant="outline" className="font-semibold">
-                      Exp: {calcExp(item.dateOfResigning, item.dateOfJoining)}
-                      {"  "}
-                      yrs
-                    </Badge>
-                  )}
-                </CardTitle>
+            <React.Fragment key={item.id}>
+              <Dialog>
+                <div className="bg-card shadow rounded-lg border p-4 max-w-sm mb-2">
+                  <div>
+                    <div className="flex items-center gap-3 text-base font-semibold">
+                      {item.jobTitle}{" "}
+                      {item.current && (
+                        <Badge variant="default" className="font-semibold">
+                          Current
+                        </Badge>
+                      )}
+                      {item.current ? (
+                        <Badge variant="outline" className="font-semibold">
+                          Exp:{" "}
+                          {calcExp(
+                            format(new Date(), "yyyy-MM-dd"),
+                            item.dateOfJoining
+                          )}{" "}
+                          yrs
+                        </Badge>
+                      ) : (
+                        <Badge variant="outline" className="font-semibold">
+                          Exp:{" "}
+                          {calcExp(item.dateOfResigning, item.dateOfJoining)}
+                          {"  "}
+                          yrs
+                        </Badge>
+                      )}
+                    </div>
 
-                <div className="space-y-4">
-                  <p className="mb-2 text-sm text-gray-600 dark:text-white">
-                    {item.company}
-                  </p>
-                  <span>
-                    <p className="mb-2 text-base font-semibold text-black dark:text-white">
-                      Job Description:
-                    </p>
+                    <div className="space-y-4">
+                      <p className="mb-2 text-sm text-gray-600 dark:text-white">
+                        {item.company}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row items-center justify-between">
+                    <div className="text-sm">
+                      {item.workingFrom}{" "}
+                      {item.workingTill && `- ${item.workingTill}`}
+                    </div>
+                    <div>
+                      <DialogTrigger asChild>
+                        <Button variant="default" size="icon">
+                          <Maximize2 className="size-4" />
+                        </Button>
+                      </DialogTrigger>
+                    </div>
+                  </div>
+                </div>
+
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{item.jobTitle}</DialogTitle>
+                    <DialogDescription>
+                      <span className="font-semibold text-sm text-gray-500">
+                        {item.company}
+                      </span>
+                      {item.current && (
+                        <Badge variant="default" className="font-semibold ml-2">
+                          Current
+                        </Badge>
+                      )}
+                      {item.current ? (
+                        <Badge variant="outline" className="font-semibold ml-2">
+                          Exp:{" "}
+                          {calcExp(
+                            format(new Date(), "yyyy-MM-dd"),
+                            item.dateOfJoining
+                          )}{" "}
+                          yrs
+                        </Badge>
+                      ) : (
+                        <Badge variant="outline" className="font-semibold ml-2">
+                          Exp:{" "}
+                          {calcExp(item.dateOfResigning, item.dateOfJoining)}
+                          {"  "}
+                          yrs
+                        </Badge>
+                      )}
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="space-y-4">
                     <ul className="list-inside list-disc space-y-3 text-black dark:text-white">
                       {item.jobDescription.map((item, index) => (
                         <li key={index} className="text-sm">
@@ -76,16 +140,17 @@ export default function Experience() {
                         </li>
                       ))}
                     </ul>
-                  </span>
-                </div>
-              </CardHeader>
-              <CardFooter className="text-sm">
-                {item.workingFrom} {item.workingTill && `- ${item.workingTill}`}
-              </CardFooter>
-            </Card>
-          )
+                  </div>
+                  <div className="text-sm">
+                    {item.workingFrom}{" "}
+                    {item.workingTill && `- ${item.workingTill}`}
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </React.Fragment>
+          );
         })}
       </div>
     </section>
-  )
+  );
 }
